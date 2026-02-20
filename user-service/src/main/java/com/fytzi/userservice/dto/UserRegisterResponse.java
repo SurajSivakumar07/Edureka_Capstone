3d82e0f9e58005078adb6ddcea5afe73efa0f33a
SurajSivakumar07 <surajsuri279@gmail.com>
2026-02-21 00:37:37 +0530

    config server changes

--- user-service/src/main/java/com/fytzi/userservice/dto/UserRegisterResponse.java
+++ user-service/src/main/java/com/fytzi/userservice/dto/UserRegisterResponse.java
@@ -1,10 +1,13 @@
 package com.fytzi.userservice.dto;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UserRegisterResponse {
     private String email;
